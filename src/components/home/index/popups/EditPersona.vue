7421cd1f63402df57c8f54eec661983e8b0ca292
juan jose <juan.jara@campusucc.edu.co>
2023-10-25 23:13:31 -0500

    agregación patch para persona update

--- src/components/home/index/popups/EditPersona.vue
+++ src/components/home/index/popups/EditPersona.vue
@@ -1,5 +1,5 @@
 <template>
-    <form action="">
+    <form action="" @submit.prevent="updateperson">
 
         <div class="row">
             <div class="column">
@@ -7,7 +7,7 @@
 
             </div>
             <div class="column">
-                <input v-model="persona.nombre" type="text" class="form-control"  >
+                <input v-model="persona.nombre" type="text" class="form-control">
 
             </div>
         </div>
@@ -18,7 +18,7 @@
 
             </div>
             <div class="column">
-                <input type="text" v-model="persona.apellido" class="form-control"  >
+                <input type="text" v-model="persona.apellido" class="form-control">
 
             </div>
         </div>
@@ -29,7 +29,7 @@
 
             </div>
             <div class="column">
-                <input type="date" v-model="persona.fecha_nacimiento" class="form-control"  >
+                <input type="date" v-model="persona.fecha_nacimiento" class="form-control">
 
             </div>
         </div>
@@ -53,13 +53,13 @@
 
             </div>
             <div class="column">
-                <input type="text" v-model="persona.img" class="form-control"  >
+                <input type="text" v-model="persona.img" class="form-control">
 
             </div>
         </div>
         <hr>
         <div class="row">
-            <button class="btn btn-success" style="background-color: blueviolet;" @click="updateperson()" ><font-awesome-icon
+            <button class="btn btn-success" style="background-color: blueviolet;" type="submit"><font-awesome-icon
                     :icon="['fas', 'floppy-disk']" /> Guardar cambios</button>
         </div>
 
@@ -68,25 +68,70 @@
 </template>
 
 <script>
+import axios from 'axios';
+import Swal from 'sweetalert2'
 export default {
     name: 'editperson',
     props: {
         persona: Object
     },
     methods: {
+
+
         updateperson() {
 
+            const personajson = {
+                "nombre": this.persona.nombre,
+                "apellido": this.persona.apellido,
+                "fecha_nacimiento": this.persona.fecha_nacimiento,
+                "img": this.persona.img
 
-            alert('esta es la url de la imagen'+this.persona.nombre);
+            }
+            try {
+                axios.defaults.headers.common['apikey'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicGRjcHNsbnlzenZhZXBsaXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzMjI1NzcsImV4cCI6MjAwMTg5ODU3N30.hK68jPYXFIDV86TjlROSx2KTKkr2lFJuKBV2kVn3tMM'
+                axios.patch('https://sbpdcpslnyszvaepliyp.supabase.co/rest/v1/Persona?id=eq.' + this.persona.id, personajson).then(response => {
+                    console.log(response.status);
+
+
+                    if (response.status == 204) {
+                        Swal.fire(
+                            'Se han guardado cambios!',
+                            'Solicitud procesada con exito',
+                            'success'
+                        )
+
+                    } else {
+
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Parece que ha ocurrido algo, intentalo mas tarde',
+
+                        })
+
+                    }
+
+                })
+
+            } catch {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Parece que ha ocurrido algo, intentalo mas tarde',
+
+                })
+
+            }
+
 
         }
     },
-    data(){
-        return{
-            nombre:'',
-            apellido:'',
-            fecha_nacimiento:'',
-            img:''
+    data() {
+        return {
+            nombre: '',
+            apellido: '',
+            fecha_nacimiento: '',
+            img: ''
 
         }
 

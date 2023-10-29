fe30141d56b29a9e33b08cfea12adc746da6ff92
juan jose <juan.jara@campusucc.edu.co>
2023-10-28 19:07:38 -0500

    agregacion enrutador dentro del modal

---
+++ src/components/home/index/popups/modalRouter/index.vue
@@ -0,0 +1,14 @@
+<template>
+<h4>index del modal router</h4>
+<p>Este es el parametro {{ id }} </p>
+
+</template>
+
+
+<script>
+export default {
+    name:'hommodal',
+   props:['id']
+}
+
+</script>
---
+++ src/components/home/index/popups/modalRouter/promedioej.vue
@@ -0,0 +1,13 @@
+<template>
+<h4>otra ruta dentro del modal {{ id }}</h4>
+
+
+
+</template>
+
+<script>
+export default{
+    name : 'promej',
+   props:['id']
+}
+</script>

2aafe28e5c6f2f559db86ce445bad56b13bbfb58
juan jose <juan.jara@campusucc.edu.co>
2023-10-30 16:47:15 -0500

    agregacion edit ejercicio

--- src/components/home/index/popups/editejercicio.vue
+++ src/components/home/index/popups/editejercicio.vue
@@ -1,5 +1,5 @@
 <template>
-    <form>
+    <form @submit.prevent="form">
         <div class="row">
             <label>Nombre:</label>
             <input type="text" v-model="ejercicio.nombre" class="form-control">
@@ -24,12 +24,12 @@
             <label>Img:</label>
             <input type="text" v-model="ejercicio.img" class="form-control">
             <div class="row text-center">
-                <img :src="ejercicio.img" style="width: 270px;"  alt="" class="">
+                <img :src="ejercicio.img" style="width: 270px;" alt="" class="">
             </div>
         </div>
         <br>
         <div class="row">
-            <button class="btn" style="background-color: blueviolet; color: aliceblue;">Guardar</button>
+            <button class="btn" style="background-color: blueviolet; color: aliceblue;" type="submit">Guardar</button>
         </div>
 
     </form>
@@ -37,11 +37,42 @@
 
 
 <script>
+import axios from 'axios';
+import Swal from 'sweetalert2';
 export default {
     name: 'editejercicio',
     props: {
         ejercicio: {
             type: Object
+        }
+    },
+    methods: {
+        form() {
+            try {
+                let ejercicioup = {
+                    "nombre": this.ejercicio.nombre,
+                    "descripcion": this.ejercicio.descripcion,
+                    "series": this.ejercicio.series,
+                    "repeticiones": this.ejercicio.repeticiones,
+                    "img": this.ejercicio.img
+                }
+
+
+
+                axios.defaults.headers.common['apikey'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicGRjcHNsbnlzenZhZXBsaXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzMjI1NzcsImV4cCI6MjAwMTg5ODU3N30.hK68jPYXFIDV86TjlROSx2KTKkr2lFJuKBV2kVn3tMM'
+                axios.patch('https://sbpdcpslnyszvaepliyp.supabase.co/rest/v1/ejercicio?id=eq.' + this.ejercicio.id, ejercicioup).then(response => {
+                    console.log(response.status)
+                    if (response.status == 204) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Exito!',
+                            text: 'Se ha guardo correctamente la información'
+                        })
+                    }
+                })
+            } catch {
+
+            }
         }
     }
 }
